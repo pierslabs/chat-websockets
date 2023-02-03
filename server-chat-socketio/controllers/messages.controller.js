a7e1caaf9ecd02dc9340c2f9c51762075f585da0
pierslabs <pedrolosasp@gmail.com>
2023-02-03 09:25:53 +0100

    BE:WIP Added messages routes && define socket actions.

--- server-chat-socketio/controllers/messages.controller.js
+++ server-chat-socketio/controllers/messages.controller.js
@@ -1,9 +1,23 @@
-const getMessages = (req, res) => {
+const Message = require('../models/messageSchema');
+
+const getMessages = async (req, res) => {
   const uid = req.uid;
+  const messageFrom = req.params.from;
+
+  const last35Messages = await Message.find({
+    $or: [
+      { to: uid, from: messageFrom },
+      { to: messageFrom, from: uid },
+    ],
+  })
+    .sort({ created_at: 'desc' })
+    .limit(30);
+
   res.json({
     ok: true,
-    msg: 'messgaes',
+    msg: last35Messages,
     uid,
+    messageFrom,
   });
 };
 
--- server-chat-socketio/models/sockets.js
+++ server-chat-socketio/models/sockets.js
@@ -9,6 +9,18 @@ class Sockets {
     // On connection
     this.io.on('connection', (socket) => {
       console.log('client conected 😄!!');
+
+      // TODO: Validate token
+
+      // TODO: get active users 'uid'
+
+      // TODO Emit all conected users
+
+      // TODO: Socket join  uid,  hall
+
+      // TODO listen user emit msg
+
+      // TODO: disconect user  'DB'
     });
   }
 }
